362ede8f15874a4c0fabb98062b71b1906a93b3a
David Freina <d.freina@student.vu.nl>
2022-11-15 15:34:00 +0100

    added code to reproduce fig 1c finished topology implementations added __pycache__, settings.json to .gitignore
    Co-authored-by: Jonas Wagner <WagnerBot@users.noreply.github.com>

--- lab2/reproduce_1c.py
+++ lab2/reproduce_1c.py
@@ -13,14 +13,98 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import matplotlib.pyplot as plt
+import numpy as np
 import topo
+
+# if __name__ == "__main__":
+#     print("main")
 
 # Same setup for Jellyfish and Fattree
 num_servers = 686
 num_switches = 245
 num_ports = 14
 
+
 ft_topo = topo.Fattree(num_ports)
-jf_topo = topo.Jellyfish(num_servers, num_switches, num_ports)
 
 # TODO: code for reproducing Figure 1(c) in the jellyfish paper
+
+ft_distances = {}
+# use array from 0..6 for fraction calculation
+ft_path_lengths = [0] * 7
+
+ft_switches = ft_topo.switches.copy()
+
+for src_sw_srv in ft_topo.switches:
+    distances, _ = topo.dijkstra(src_sw_srv, ft_switches, ft_topo.servers)
+    for dest_sw_srv in distances:
+        ft_distances[(src_sw_srv.id, dest_sw_srv.id)] = distances[dest_sw_srv]
+        # For edge switch add the reverse route as well
+        if src_sw_srv.type == "edge-sw":
+            ft_distances[(dest_sw_srv.id, src_sw_srv.id)
+                         ] = distances[dest_sw_srv]
+    # remove edge switch because it is not needed anymore
+    # significant speed up achieved
+    if src_sw_srv.type == "edge-sw":
+        ft_switches.remove(src_sw_srv)
+
+
+ft_edge_switches = [server.edges[0].lnode.id for server in ft_topo.servers]
+
+for idx, curr_edge_sw in enumerate(ft_edge_switches):
+    for other_edge_sw in ft_edge_switches[idx + 1:]:
+        # Add 2 for connection from host -- path -- host
+        host_host_distance = ft_distances[(curr_edge_sw, other_edge_sw)] + 2
+        ft_path_lengths[host_host_distance] += 1
+
+
+ft_path_length_distribution = [
+    length / sum(ft_path_lengths[2:7]) for length in ft_path_lengths[2:7]]
+
+
+# use array from 0..6 for fraction calculation
+jf_path_lengths = [0] * 7
+
+for i in range(1):
+    jf_topo = topo.Jellyfish(num_servers, num_switches, num_ports)
+    jf_distances = {}
+
+    for src_sw_srv in jf_topo.switches:
+        distances, _ = topo.dijkstra(
+            src_sw_srv, jf_topo.switches, jf_topo.servers)
+        for dest_sw_srv in distances:
+            jf_distances[(src_sw_srv.id, dest_sw_srv.id)
+                         ] = distances[dest_sw_srv]
+            # jf_path_lengths[distances[dest_sw_srv]] += 1
+
+    jf_switches = [server.edges[0].lnode.id for server in jf_topo.servers]
+    for idx, curr_edge_sw in enumerate(jf_switches):
+        for other_edge_sw in jf_switches[idx + 1:]:
+            # Add 2 for connection from host -- path -- host
+            host_host_distance = jf_distances[(
+                curr_edge_sw, other_edge_sw)] + 2
+            jf_path_lengths[host_host_distance] += 1
+
+jf_path_length_distribution = [
+    length / sum(jf_path_lengths[2:7]) for length in jf_path_lengths[2:7]]
+
+# See: https://matplotlib.org/stable/gallery/lines_bars_and_markers/barchart.html#sphx-glr-gallery-lines-bars-and-markers-barchart-py
+labels = ['2', '3', '4', '5', '6']
+x = np.arange(len(labels))
+width = 0.35
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x + width/2, ft_path_length_distribution,
+                width, label="Fat-tree")
+rects2 = ax.bar(x - width/2, jf_path_length_distribution,
+                width, label="Jellyfish")
+
+plt.ylim([0, 1])
+ax.set_ylabel("Path length")
+ax.set_title("Path length distribution for num_ports={}".format(num_ports))
+ax.set_xticks(x, labels)
+ax.legend()
+
+fig.tight_layout()
+fig.savefig("1c.png")
--- lab2/topo.py
+++ lab2/topo.py
@@ -16,206 +16,332 @@
 import sys
 import random
 import queue
+import matplotlib.pyplot as plt
+import networkx as nx
+from matplotlib.patches import ConnectionPatch
+from itertools import count
 
 # Class for an edge in the graph
+
+
 class Edge:
-	def __init__(self):
-		self.lnode = None
-		self.rnode = None
+    def __init__(self):
+        self.lnode = None
+        self.rnode = None
 
-	def __repr__(self):
-		return "\n\t\tEdge({}, {})".format(self.lnode.id, self.rnode.id)
+    def __repr__(self):
+        return "\n\t\tEdge({}, {})".format(self.lnode.id, self.rnode.id)
 
-	def __str__(self):
-		return "\n\t\tEdge({}, {})".format(self.lnode.id, self.rnode.id)
+    def __str__(self):
+        return "\n\t\tEdge({}, {})".format(self.lnode.id, self.rnode.id)
 
-	def remove(self):
-		self.lnode.edges.remove(self)
-		self.rnode.edges.remove(self)
-		self.lnode = None
-		self.rnode = None
+    def remove(self):
+        self.lnode.edges.remove(self)
+        self.rnode.edges.remove(self)
+        self.lnode = None
+        self.rnode = None
 
 # Class for a node in the graph
+
+
 class Node:
-	def __init__(self, id, type):
-		self.edges = []
-		self.id = id
-		self.type = type
+    def __init__(self, id, type):
+        self.edges = []
+        self.id = id
+        self.type = type
 
-	def __repr__(self):
-		return "\tNode({}, {}, {})\n".format(self.id, self.type, self.edges)
+    def __repr__(self):
+        # return "Node({})".format(self.id, self.type, self.edges)
+        return "\tNode({}, {}, {})\n".format(self.id, self.type, self.edges)
 
-	def __str__(self):
-		return "\tNode({}, {}, {})\n".format(self.id, self.type, self.edges)
+    def __str__(self):
+        return "\tNode({}, {}, {})\n".format(self.id, self.type, self.edges)
 
-	# Add an edge connected to another node
-	def add_edge(self, node):
-		edge = Edge()
-		edge.lnode = self
-		edge.rnode = node
-		self.edges.append(edge)
-		node.edges.append(edge)
-		return edge
+    # Add an edge connected to another node
+    def add_edge(self, node):
+        edge = Edge()
+        edge.lnode = self
+        edge.rnode = node
+        self.edges.append(edge)
+        node.edges.append(edge)
+        return edge
 
-	# Remove an edge from the node
-	def remove_edge(self, edge):
-		self.edges.remove(edge)
+    # Remove an edge from the node
+    def remove_edge(self, edge):
+        self.edges.remove(edge)
 
-	# Decide if another node is a neighbor
-	def is_neighbor(self, node):
-		for edge in self.edges:
-			if edge.lnode == node or edge.rnode == node:
-				return True
-		return False
+    # Decide if another node is a neighbor
+    def is_neighbor(self, node):
+        for edge in self.edges:
+            if edge.lnode == node or edge.rnode == node:
+                return True
+        return False
 
 
 class Jellyfish:
 
-	def __init__(self, num_servers, num_switches, num_ports):
-		self.servers = []
-		self.switches = []
-		self.generate(num_servers, num_switches, num_ports)
+    def __init__(self, num_servers, num_switches, num_ports):
+        self.servers = []
+        self.switches = []
+        self.generate(num_servers, num_switches, num_ports)
 
-	def generate(self, num_servers, num_switches, num_ports):
+    def generate(self, num_servers, num_switches, num_ports):
 
-		# TODO: code for generating the jellyfish topology
-		# each switch i has k_i ports, where r_i is the connection to other switches and remaining k_i - r_i ports are used to connect to servers
-		# degree is number of ports per switch
-		degree = num_switches * num_ports // num_servers
-		max_server_per_switch = num_ports - degree
+        # TODO: code for generating the jellyfish topology
+        # each switch i has k_i ports, where r_i is the connection to other switches and remaining k_i - r_i ports are used to connect to servers
+        # degree is number of ports per switch
+        degree = num_switches * num_ports // num_servers
+        max_server_per_switch = num_ports - degree
+        server_per_switch = - (num_servers // - num_switches)
 
-		edge_cnt, switch_id = 0
+        edge_cnt = 0
 
-		for server in range(num_servers):
-			num_connections = random.randrange(max_server_per_switch)
+        for switch_id in range(num_switches):
+            switch_node = Node(switch_id, "switch")
 
-			switch_node = Node(switch_id, "tor_switch")
+            if(edge_cnt < num_servers):
+                for server_id in range(server_per_switch):
+                    if(edge_cnt + server_id == num_servers):
+                        break
+                    server_node = Node(server_id + edge_cnt, "server")
+                    switch_node.add_edge(server_node)
+                    self.servers.append(server_node)
 
-			for edg in range(num_connections):
-				server_node = Node(server + edg + edge_cnt, "server")
-				switch_node.add_edge(server_node)
-				self.servers[server + edg + edge_cnt] = server_node
+            self.switches.append(switch_node)
+            edge_cnt += server_per_switch
 
-			self.switches[switch_id] = switch_node
+        # # Initialize servers and top of the rack switches
+        # while(edge_cnt < num_servers):
+        # 	num_connections = random.randrange(1, max_server_per_switch)
 
-			switch_id += 1
-			edge_cnt += num_connections
+        # 	switch_node = Node(switch_id, "tor_switch")
 
-			if(edge_cnt == num_servers):
-				while(switch_id < num_switches):
-					self.switches[switch_id] = Node(switch_id, "net_switch")
-					switch_id += 1
-				break
+        # 	edg = 0
+        # 	if(num_connections + edge_cnt >= num_servers):
+        # 		edg = num_connections + edge_cnt - num_servers
 
+        # 	for tmp in range(num_connections - edg):
+        # 		server_node = Node(tmp + edge_cnt, "server")
+        # 		switch_node.add_edge(server_node)
+        # 		self.servers.append(server_node)
+        # 		if(num_connections + tmp == num_servers):
+        # 			break
 
+        # 	self.switches.append(switch_node)
 
+        # 	switch_id += 1
+        # 	edge_cnt += num_connections
 
-		while(True):
+        # # Add remaining switches without any connections to the dict
+        # while(switch_id < num_switches):
+        # 	self.switches.append(Node(switch_id, "net_switch"))
+        # 	switch_id += 1
 
+        switches_to_connect = len(self.switches)
 
+        # Add free switches randomly to the network
+        while(switches_to_connect > 2):
+            while(True):
+                node1 = random.choice(self.switches)
+                node2 = random.choice(self.switches)
+                if((len(node1.edges) >= num_ports) or (len(node2.edges) >= num_ports) or node1.is_neighbor(node2)):
+                    continue
+                else:
+                    break
 
+            node1.add_edge(node2)
+            if(len(node1.edges) >= num_ports):
+                switches_to_connect -= 1
+            if(len(node2.edges) >= num_ports):
+                switches_to_connect -= 1
 
+        remaining_switches = []
+        for switch_id in range(num_switches):
+            switch = self.switches[switch_id]
+            if(len(switch.edges) < num_ports):
+                remaining_switches.append(switch)
 
+        # Connect last free switches to network until only one switch with one open port remains
+        while(len(remaining_switches) > 1):
+
+            tmp = random.choice(self.switches)
+            sw_to_connect = remaining_switches[0]
+
+            if(tmp == sw_to_connect):
+                continue
+
+            edg_to_remove = random.choice(tmp.edges)
+            sw_to_connect.add_edge(edg_to_remove.lnode)
+            sw_to_connect.add_edge(edg_to_remove.rnode)
+
+            tmp.remove_edge(edg_to_remove)
+
+            if(len(sw_to_connect.edges) >= num_ports):
+                remaining_switches.remove(sw_to_connect)
+
+        # self.plot()
+
+    def plot(self):
+        nodes = []
+        edgs = []
+        for i in range(10):
+            nd = random.choice(self.switches)
+            if(nd not in nodes):
+                nodes.append(nd)
+
+        for i in range(len(nodes)):
+            nd = nodes[i]
+            for j in range(len(nd.edges)):
+                tpl = (nd.edges[j].lnode.id, nd.edges[j].rnode.id)
+                edgs.append(tpl)
+
+        JF_GRAPH = nx.Graph(edgs)
+        pos = nx.spring_layout(JF_GRAPH)
+        nx.draw(JF_GRAPH, pos, with_labels=True)
+        plt.savefig("jf.png")
 
 
 class Fattree:
 
-	def __init__(self, num_ports):
-		self.servers = []
-		self.switches = []
-		self.generate(num_ports)
+    def __init__(self, num_ports):
+        self.servers = []
+        self.switches = []
+        self.generate(num_ports)
 
-	def generate(self, num_ports):
-		# TODO: code for generating the fat-tree topology
-		num_pods = num_ports
-		num_hosts = (num_pods ** 3) / 4
-		num_switches_per_layer = num_pods // 2
-		num_core_switches = (num_pods // 2) ** 2
+    def generate(self, num_ports):
+        # TODO: code for generating the fat-tree topology
+        PLOT_NODE_SPACING = 2
 
-		# core_switches = {}
+        num_pods = num_ports
+        num_hosts = (num_pods ** 3) / 4
+        num_switches_per_layer = num_pods // 2
+        num_core_switches = (num_pods // 2) ** 2
 
-		pod_switches = {}
+        plot_nodes = {}
+        plot_core_sw_x = (num_pods * num_switches_per_layer * PLOT_NODE_SPACING -
+                          PLOT_NODE_SPACING * 1.5) / 2 - (num_core_switches / 2 - 1)
+        plot_agg_sw_x_offset = 0
 
-		for sw_core_row_idx in range(0, num_core_switches // num_switches_per_layer):
-			core_switches = []
-			for sw_core_column_idx in range(0, num_switches_per_layer):
-				core_sw_id = "10.{}.{}.{}".format(num_pods, sw_core_row_idx + 1, sw_core_column_idx + 1)
-				core_sw = Node(core_sw_id, "core-sw")
-				core_switches.append(core_sw)
-				self.switches[core_sw_id] = core_sw
+        # core_switches = {}
 
-			for pod_idx in range(0, num_pods):
-				pod_switches.setdefault(pod_idx, {"agg-sw": [], "edge-sw": []})
+        pod_switches = {}
+        # fig, ax = plt.subplots(figsize=(num_pods * num_switches_per_layer * PLOT_NODE_SPACING * 2, num_core_switches))
+        # ax.set_xlim(-1, num_pods * num_switches_per_layer * PLOT_NODE_SPACING)
+        # ax.set_ylim(0, 4)
+        # plt.axis('off')
 
-				agg_sw_id = "10.{}.{}.{}".format(pod_idx, num_switches_per_layer + sw_core_row_idx, 1)
-				agg_sw = Node(agg_sw_id, "agg-sw")
-				for core_sw in core_switches:
-					core_sw.add_edge(agg_sw)
-					# agg_sw.add_edge(core_sw)
-				pod_switches[pod_idx]["agg-sw"].append(agg_sw)
-				self.switches[agg_sw_id] = agg_sw
+        for sw_core_row_idx in range(0, num_core_switches // num_switches_per_layer):
+            core_switches = []
+            for sw_core_column_idx in range(0, num_switches_per_layer):
+                core_sw_id = "10.{}.{}.{}".format(
+                    num_pods, sw_core_row_idx + 1, sw_core_column_idx + 1)
+                core_sw = Node(core_sw_id, "core-sw")
+                core_switches.append(core_sw)
+                self.switches.append(core_sw)
+                # plot_core_sw = ax.annotate(core_sw_id, xy=(plot_core_sw_x, 4), xycoords="data",
+                # 					va="center", ha="center",
+                # 					bbox=dict(boxstyle="round", fc="w"))
+                # plot_nodes[core_sw_id] = plot_core_sw
+                # plot_core_sw_x += 1
 
-				edge_sw_id = "10.{}.{}.{}".format(pod_idx, sw_core_row_idx, 1)
-				edge_sw = Node(edge_sw_id, "edge-sw")
-				# agg_sw.add_edge(edge_sw)
-				# edge_sw.add_edge(agg_sw)
-				pod_switches[pod_idx]["edge-sw"].append(edge_sw)
-				self.switches[edge_sw_id] = edge_sw
+            for pod_idx in range(0, num_pods):
+                pod_switches.setdefault(pod_idx, {"agg-sw": [], "edge-sw": []})
 
-				for srv_idx in range(1, (num_pods // 2) + 1):
-					srv_id = "10.{}.{}.{}".format(pod_idx, sw_core_row_idx, srv_idx + 1)
-					srv = Node(srv_id, "server")
-					self.servers[srv_id] = srv
-					edge_sw.add_edge(srv)
+                agg_sw_id = "10.{}.{}.{}".format(
+                    pod_idx, num_switches_per_layer + sw_core_row_idx, 1)
+                agg_sw = Node(agg_sw_id, "agg-sw")
+
+                # plot_agg_sw_base_x = pod_idx * num_switches_per_layer * PLOT_NODE_SPACING
+
+                # plot_agg_sw = ax.annotate(agg_sw_id, xy=(plot_agg_sw_base_x + plot_agg_sw_x_offset, 3), xycoords="data",
+                # 				va="center", ha="center",
+                # 				bbox=dict(boxstyle="round", fc="w"))
+                # plot_nodes[agg_sw_id] = plot_agg_sw
+
+                for core_sw in core_switches:
+                    core_sw.add_edge(agg_sw)
+                    # con = ConnectionPatch(plot_nodes[core_sw.id].xy, plot_agg_sw.xy, "data", "data", arrowstyle="->", shrinkA=2, shrinkB=2,
+                    # 		mutation_scale=1, fc="w")
+                    # ax.add_artist(con)
+
+                pod_switches[pod_idx]["agg-sw"].append(agg_sw)
+                self.switches.append(agg_sw)
+
+                edge_sw_id = "10.{}.{}.{}".format(pod_idx, sw_core_row_idx, 1)
+                edge_sw = Node(edge_sw_id, "edge-sw")
+
+                # plot_edge_sw = ax.annotate(edge_sw_id, xy=(plot_agg_sw_base_x + plot_agg_sw_x_offset, 2), xycoords="data",
+                # 		va="center", ha="center",
+                # 		bbox=dict(boxstyle="round", fc="w"))
+                # plot_nodes[edge_sw_id] = plot_edge_sw
+
+                # agg_sw.add_edge(edge_sw)
+                # edge_sw.add_edge(agg_sw)
+                pod_switches[pod_idx]["edge-sw"].append(edge_sw)
+                self.switches.append(edge_sw)
+
+                for srv_idx in range(1, (num_pods // 2) + 1):
+                    srv_id = "10.{}.{}.{}".format(
+                        pod_idx, sw_core_row_idx, srv_idx + 1)
+                    srv = Node(srv_id, "server")
+                    # plot_srv = ax.annotate(srv_id, xy=(-1.5 + srv_idx + plot_agg_sw_base_x + plot_agg_sw_x_offset, 1), xycoords="data",
+                    # 		va="center", ha="center",
+                    # 		bbox=dict(boxstyle="round", fc="w"))
+                    # con = ConnectionPatch(plot_nodes[edge_sw_id].xy, plot_srv.xy, "data", "data", arrowstyle="->", shrinkA=2, shrinkB=2,
+                    # 		mutation_scale=1, fc="w")
+                    # ax.add_artist(con)
+                    self.servers.append(srv)
+                    edge_sw.add_edge(srv)
+
+            plot_agg_sw_x_offset += PLOT_NODE_SPACING
+
+        for pod in pod_switches:
+            for agg_sw in pod_switches[pod]["agg-sw"]:
+                for edge_sw in pod_switches[pod]["edge-sw"]:
+                    agg_sw.add_edge(edge_sw)
+                    # con = ConnectionPatch(plot_nodes[agg_sw.id].xy, plot_nodes[edge_sw.id].xy, "data", "data", arrowstyle="->", shrinkA=2, shrinkB=2,
+                    # 		mutation_scale=1, fc="w")
+                    # ax.add_artist(con)
+
+        # fig.savefig("test.png")
 
 
-		for pod in pod_switches:
-			for agg_sw in pod_switches[pod]["agg-sw"]:
-				for edge_sw in pod_switches[pod]["edge-sw"]:
-					agg_sw.add_edge(edge_sw)
-					# edge_sw.add_edge(agg_sw)
+def dijkstra(start_node, switches, servers):
+    # unvisited = {**{node: {"weight": None, "path": []} for node in switches}, **{node: {"weight": None, "path": []} for node in servers}}
+    # unvisited = {node: {"weight": None, "path": []} for node in switches}
+    # unvisited[start_node] = {"weight": 0, "path": []}
+    unvisited = {node: None for node in switches}
+    # unvisited = {**{node: None for node in switches}, **{node: None for node in servers}}
+    unvisited[start_node] = 0
+    path = {}
+    visited = []
 
-			# ID of core switch definition:
-			# 10.k.j.i, where j and i denote that switches
-			# coordinates in the (k/2)^2 core switch grid
-			# (each in [1, (k/2)], starting from top-left).
-			# calculate j:
-			# sw_core_row = sw_core_idx // num_switches_per_layer + 1
-			# calculate i:
-			# sw_core_column = sw_core_idx % num_switches_per_layer + 1
+    unique = count()
+    pqueue = queue.PriorityQueue()
+    pqueue.put((0, next(unique), start_node))
 
-			# create core switch id and add it to dictionary
-			# with array containing the pod switch id's for each core sw
-			# core_sw_id = "10.{}.{}.{}".format(num_pods, sw_core_row, sw_core_column)
-			# core_sw = Node(core_sw_id, "core-sw")
-#			core_switches.setdefault(core_sw_id, set())
+    while not pqueue.empty():
+        current_node = pqueue.get()[2]
+        visited.append(current_node)
 
-			# iterate over pods and add pod sw id's to core sw array
-			# also add pod sw id to dictionary with pod index as key
-			# for pod_idx in range(0, num_pods):
-			# 	pod_sw_id = "10.{}.{}.{}".format(pod_idx, num_switches_per_layer + sw_core_row - 1, 1)
-			# 	core_switches[core_sw_id].add(pod_sw_id)
+        # check switches to find next possible hop which was not yet visited
+        for neighbor in (switches):
+            if current_node.is_neighbor(neighbor) and neighbor not in visited:
+                # check if
+                # old_weight = unvisited[neighbor]["weight"]
+                old_weight = unvisited[neighbor]
 
-			# print(core_sw_id)
+                # use hard-coded 1 because we have no weighted links
+                # new_weight = unvisited[current_node]["weight"] + 1
+                new_weight = unvisited[current_node] + 1
 
-		# for pod_idx in range(0, num_pods):
-		# 	# print("POD {}".format(pod_idx))
-		# 	for sw_pod_idx in range(0, num_pods):
-		# 		pod_sw_id = "10.{}.{}.{}".format(pod_idx, sw_pod_idx, 1)
-		# 		pod_sw = Node(core_sw_id, "pod-sw")
-		# 		# pod_switches.setdefault(pod_sw_id, set())
-		# 		for sw_pod_link in range(0, num_switches_per_layer):
-		# 			if sw_pod_idx // num_switches_per_layer == 0:
-		# 				pod_sw_link = "10.{}.{}.{}".format(pod_idx, num_switches_per_layer + sw_pod_link, 1)
-		# 			else:
-		# 				pod_sw_link = "10.{}.{}.{}".format(pod_idx, sw_pod_link, 1)
-		# 			pod_switches[pod_sw_id].add(pod_sw_link)
-				# print(pod_sw_id)
-
-		# print(core_switches)
-		# print(pod_switches)
-
-		# self.switches = {**core_switches, **pod_switches}
-
-		for switch in switches:
-			print(switch)
+                if old_weight is None or new_weight < old_weight:
+                    pqueue.put((new_weight, next(unique), neighbor))
+                    # unvisited[neighbor]["weight"] = new_weight
+                    unvisited[neighbor] = new_weight
+                    # unvisited[neighbor]["path"] = unvisited[current_node]["path"].copy()
+                    # unvisited[neighbor]["path"].append(current_node)
+                    # save path for reconstruction
+                    path[neighbor] = current_node
+        pqueue.task_done()
+    return unvisited, path
